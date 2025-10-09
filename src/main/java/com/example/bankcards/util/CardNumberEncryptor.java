bbd99962b635306af076081c831e714d1f416608
tg <eat.some.trouble@gmail.com>
2025-10-09 14:50:38 +0300

    feat: ✨ added CardNumberEncryptor

--- src/main/java/com/example/bankcards/util/CardNumberEncryptor.java
+++ src/main/java/com/example/bankcards/util/CardNumberEncryptor.java
@@ -3,31 +3,31 @@ package com.example.bankcards.util;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 
 @Converter
+@Component
 public class CardNumberEncryptor implements AttributeConverter<String, byte[]> {
 
-    @Value("${card-encryption.secret}")
-    private static String SECRET;
-
-    @Value("${card-encryption.algorithm}")
-    private static String ALGORITHM;
+    private final String algorithm;
 
     private final SecretKeySpec secretKeySpec;
 
-    public CardNumberEncryptor() {
-        this.secretKeySpec = new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), "AES");
+    public CardNumberEncryptor(@Value("${card-encryption.secret}") String secret,
+                               @Value("${card-encryption.algorithm}") String algorithm) {
+        this.secretKeySpec = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "AES");
+        this.algorithm = algorithm;
     }
 
     @Override
     public byte[] convertToDatabaseColumn(String attribute) {
         if (attribute == null) return null;
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher = Cipher.getInstance(algorithm);
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             return cipher.doFinal(attribute.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
@@ -39,7 +39,7 @@ public class CardNumberEncryptor implements AttributeConverter<String, byte[]> {
     public String convertToEntityAttribute(byte[] dbData) {
         if (dbData == null) return null;
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher = Cipher.getInstance(algorithm);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
             byte[] decrypted = cipher.doFinal(dbData);
             return new String(decrypted, StandardCharsets.UTF_8);
