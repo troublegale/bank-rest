bcc260cb7b76a160c1dfc3d612afe750226745b7
tg <eat.some.trouble@gmail.com>
2025-10-09 14:49:33 +0300

    feat: ✨ added auth exception handling

---
+++ src/main/java/com/example/bankcards/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,38 @@
+package com.example.bankcards.security;
+
+import com.example.bankcards.dto.AppError;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        AppError appError = new AppError(
+                403,
+                List.of("Insufficient privileges"),
+                new Date()
+        );
+        String json = objectMapper.writeValueAsString(appError);
+        response.getWriter().write(json);
+    }
+
+}
+
